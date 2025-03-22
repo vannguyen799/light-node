78c43e663e8624bc6c24375a7195eb29a54a5e11
eq <eq@eq>
2025-03-22 19:32:27 +0700

    i

--- run_multi.sh
+++ run_multi.sh
@@ -3,24 +3,24 @@
 mapfile -t privateKeys < privateKeys.txt
 mapfile -t proxies < proxies.txt
 
-
-
 for i in "${!privateKeys[@]}"; do
     echo "Starting executor with Proxy: ${proxies[$i]}"
     pk=${privateKeys[$i]}
     proxy=${proxies[$i]}
-    env http_proxy="http://$proxy" https_proxy="http://$proxy" no_proxy="localhost,127.0.0.1" \
+    until env http_proxy="http://$proxy" https_proxy="http://$proxy" no_proxy="localhost,127.0.0.1" \
         GRPC_URL=grpc.testnet.layeredge.io:9090 \
         CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709 \
         ZK_PROVER_URL=https://layeredge.mintair.xyz \
         API_REQUEST_TIMEOUT=1000 \
         POINTS_API=https://light-node.layeredge.io \
         PRIVATE_KEY=$pk \
-        ./light-node &
+        ./light-node >> "executor_${i}.log" 2>&1
+    do 
+        echo "Retrying... Proxy: ${proxies[$i]}" >> "executor_${i}.log"
+        sleep 1
+    done &
     sleep 1
 done
 
 wait
 echo "All executors have finished."
-
-
